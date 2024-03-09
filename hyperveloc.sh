cbddfe8ee6fc20e342e56b293594d6eb833827f2
hyperveloce <30348101+hyperveloce@users.noreply.github.com>
2024-03-09 15:47:13 +1100

    Update hyperveloc.sh

--- hyperveloc.sh
+++ hyperveloc.sh
@@ -6,9 +6,17 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-
 username=$(id -u -n 1000)
 builddir=$(pwd)
+
+##Move home directory
+#  #lsblk - find drive to mount
+#sudo mkdir -p /mnt/home
+#sudo mount /dev/sda2 /mnt/home
+#sudo df -Th
+#sudo cp -aR /home/* /mnt/home
+#ls -larth /mnt/home/kanasu
+
 
 # Update packages list and update system
 apt update
