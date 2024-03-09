851d4c7b8f5826a51d889b986278684f77dd36dd
hyperveloce <30348101+hyperveloce@users.noreply.github.com>
2024-03-09 17:29:34 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -74,7 +74,7 @@ nala update
 nala install nvidia-driver firmware-misc-nonfree -y
 
 # Install java-17-amazon-corretto
-wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
-echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-nala update
-nala install java-17-amazon-corretto-jdk -y
+# wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+# echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+# nala update
+# nala install java-17-amazon-corretto-jdk -y
